8d53ba631571d25cc63d44d82cd0bea02d7dd40f
Hierophant <priv.post.acc@gmail.com>
2020-09-12 23:53:39 +0200

    PS conversion script - first version

---
+++ svg_to_gazebo_cones.ps1
@@ -0,0 +1,54 @@
+﻿[System.Xml.XmlDocument] $xdoc = new-object System.Xml.XmlDocument
+$file = Resolve-Path C:\Users\Hiero\Desktop\gokart_track.xml
+$xdoc.Load($file)
+
+$Doc = New-Object System.Xml.XmlDocument
+
+$Doc.AppendChild($Doc.CreateElement("cones"))
+
+$blue_cones = $xdoc.svg.left.ChildNodes
+
+$count = 0;
+ForEach($i in $blue_cones) {
+
+
+[xml]$child = @"
+<model name='blue_cone_$count'>
+    <pose>$($i.x) $(-1*$i.y) 0 0 0 0</pose>
+    <include>
+         <uri>model:///blue_cone</uri>
+    </include>
+</model>
+"@
+
+$count++
+
+
+$Doc.DocumentElement.AppendChild($Doc.ImportNode($child.LastChild,$true))
+}
+
+$yellow_cones = $xdoc.svg.right.ChildNodes
+$count = 0;
+
+ForEach($i in $yellow_cones) {
+
+
+[xml]$child = @"
+<model name='yellow_cone_$count'>
+    <pose>$($i.x) $(-1*$i.y) 0 0 0 0</pose>
+    <include>
+         <uri>model:///yellow_cone</uri>
+    </include>
+</model>
+"@
+
+$count++
+
+
+$Doc.DocumentElement.AppendChild($Doc.ImportNode($child.LastChild,$true))
+}
+
+
+$path = Resolve-Path C:\Users\Hiero\Desktop\test.xml
+
+$Doc.Save($path)
